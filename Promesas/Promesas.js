357e5bb19bf0a166b16dc9272f87aa3e05a92337
raul <2021171023@uteq.edu.mx>
2024-01-22 17:46:04 -0600

    init fetch 4

--- Promesas/Promesas.js
+++ Promesas/Promesas.js
@@ -1,9 +1,20 @@
-// Problema cors
+// Peticion post
 
-fetch('https://wikipedia.org')
-    .then(res => res.text())
-    .then(html => {
-        document.open();
-        document.write(html);
-        document.close();
-    })
+let user = {
+    name: 'Raul',
+    age: 26
+}
+
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(resp => resp.json())
+.then(console.log)
+.catch(error => {
+    console.log('Error en la peticion');
+    console.log(error);
+});
