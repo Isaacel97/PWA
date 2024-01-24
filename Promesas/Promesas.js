b5d0d808b3426843aa08086547c71fce42e38eca
raul <2021171023@uteq.edu.mx>
2024-01-24 17:13:56 -0600

    init 6

--- Promesas/Promesas.js
+++ Promesas/Promesas.js
@@ -1,19 +1,6 @@
-//Fetch archivos locales
-
-// Almacena de manera local una imagen dentro de este
-// proyecto en la carpeta img realiza una solicitud fetch a esa imagen
-// y muestra la imagen en el navegador
-
-
-const fetchImage = async () => {
-    try {
-        const response = await fetch('./imgLocal.jpg');
-        const blob = await response.blob();
-        const img = document.createElement('img');
-        img.setAttribute('id', 'img');
-        img.src = URL.createObjectURL(blob);
-        document.body.appendChild(img);
-    } catch (error) {
-        console.error(error);
-    }
-}
+// Solicitud a una API con Fetch
+fetch(resp => {
+    resp.json().then(data => {
+        console.log(data);
+    });
+})
